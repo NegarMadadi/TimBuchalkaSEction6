8f7eac6d91c005023e73cbbbe94aa304700ebbb3
NegarMadadi <madadi.negar@gmail.com>
2022-04-12 22:19:14 +0200

    Created Vip customer

---
+++ src/main/java/org/example/sec6/VIPCustomer.java
@@ -0,0 +1,36 @@
+package org.example.sec6;
+
+public class VIPCustomer {
+    private String name;
+    private double creditLimit;
+    private String address;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public VIPCustomer() {
+        this("Sahar", 67787.98, "Istanbul");
+    }
+
+    public VIPCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "Stockholm");
+    }
+
+    public VIPCustomer(String name, double creditLimit, String address) {
+        this.name = name;
+        creditLimit = creditLimit;
+        this.address = address;
+    }
+}
+
+
