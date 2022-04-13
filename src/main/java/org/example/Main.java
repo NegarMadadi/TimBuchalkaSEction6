b572ef77d362aaa6234e28d3ae84f6d1a4aa20c8
NegarMadadi <madadi.negar@gmail.com>
2022-04-13 16:18:54 +0200

    Created Point class and tested it in Main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.sec6.Wall;
+import org.example.sec6.Point;
 
 public class Main {
     public static void main(String[] args) {
@@ -84,16 +84,23 @@ public class Main {
 
 
         //code for testing WallArea class
-
-        Wall wall = new Wall(5, 4);
+        /*  Wall wall = new Wall(5, 4);
         System.out.println("area= " + wall.getArea());
 
         wall.setHeight(-1.5);
         System.out.println("width= " + wall.getWidth());
         System.out.println("height= " + wall.getHeight());
         System.out.println("area= " + wall.getArea());
+        */
 
 
-
+        //code for testing Point class
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
     }
 }
