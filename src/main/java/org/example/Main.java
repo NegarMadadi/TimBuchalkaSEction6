7c709ff2d8ca76079fe0956c99071013c64424cb
NegarMadadi <madadi.negar@gmail.com>
2022-04-13 17:46:00 +0200

    Created Calculator class and tested CarpetCostCalculator.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
-import org.example.sec6.Point;
+import org.example.sec6.carpetCostCalculator.Calculator;
+import org.example.sec6.carpetCostCalculator.Carpet;
+import org.example.sec6.carpetCostCalculator.Floor;
 
 public class Main {
     public static void main(String[] args) {
@@ -80,6 +82,7 @@ public class Main {
 
         VIPCustomer person3 = new VIPCustomer("Zisis", 187, "Zisis@gmail.com");
         System.out.println(person1.getName());
+          System.out.println("**********************************************************");
         */
 
 
@@ -91,10 +94,12 @@ public class Main {
         System.out.println("width= " + wall.getWidth());
         System.out.println("height= " + wall.getHeight());
         System.out.println("area= " + wall.getArea());
+          System.out.println("**********************************************************");
         */
 
 
         //code for testing Point class
+        /*
         Point first = new Point(6, 5);
         Point second = new Point(3, 1);
         System.out.println("distance(0,0)= " + first.distance());
@@ -102,5 +107,55 @@ public class Main {
         System.out.println("distance(2,2)= " + first.distance(2, 2));
         Point point = new Point();
         System.out.println("distance()= " + point.distance());
+          System.out.println("**********************************************************");
+        */
+
+
+        //code for testing carpetCostCalculator package
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new org.example.sec6.carpetCostCalculator.Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/example/sec6/carpetCostCalculator/Calculator.java
@@ -0,0 +1,15 @@
+package org.example.sec6.carpetCostCalculator;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return (floor.getArea() * carpet.getCost());
+    }
+}
