9b118664a86076250f8430a4bb81293d49b8676e
NegarMadadi <madadi.negar@gmail.com>
2022-04-13 12:32:03 +0200

    Created Car class and tested it in Main class.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import org.example.sec6.VIPCustomer;
+
+ public class Main {
+    public static void main(String[] args) {
+
+        // code for testng Car class
+        /*
+         Car porsche = new Car();
+        Car holden = new Car();
+
+        System.out.println("Model is "+ porsche.getModel());
+        porsche.setModel("Carrera");
+        System.out.println("Model is "+ porsche.getModel());
+        */
+
+
+        // code for testing simpleCalculator class
+        /*
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+        System.out.println("***********************************************");
+        */
+
+        //Code for testing  Person Class
+        /*
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("**********************************************************");
+        */
+
+        //code for testing BankAccountClass
+        /*
+        BankAccount negarsAccount = new BankAccount();// ("12345", 0, "Negar Madadi",
+        // "negar.myeah@gmail.com", "070587413");
+        System.out.println(negarsAccount.getAccountNumber());
+        System.out.println(negarsAccount.getBalance());
+
+        negarsAccount.withraw(100);
+
+        negarsAccount.deposit(50);
+        negarsAccount.withraw(100);
+
+        negarsAccount.deposit(51);
+        negarsAccount.withraw(100);
+
+        BankAccount timsAccount = new BankAccount("Tim","tim@gmail.com","1245");
+        System.out.println(timsAccount.getAccountNumber() + " name " + timsAccount.getCustomerName());
+        System.out.println("***********************************************"  );
+        */
+
+
+        //code for testing VIPCustomer class
+        /*
+         VIPCustomer person1 = new VIPCustomer();
+        System.out.println(person1.getName());
+
+        VIPCustomer person2 = new VIPCustomer("Bob", 266876.98);
+        System.out.println(person1.getName());
+
+        VIPCustomer person3 = new VIPCustomer("Zisis", 187, "Zisis@gmail.com");
+        System.out.println(person1.getName());
+        */
+
+
+    }
+}
---
+++ src/main/java/org/example/sec6/Car.java
@@ -0,0 +1,23 @@
+package org.example.sec6;
+
+public class Car {
+
+    private int doors;
+    private int wheela;
+    private String model;
+    private String engine;
+    private String colur;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        }else{
+            this.model= "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
